490decbdf6b160ce94437426f99327ac60283261
Alireza Khorramshekooh <alireza.khorramshekooh@gmail.com>
2024-11-09 11:32:54 +0330

    redis module add

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
+import { redisModule } from 'src/modules.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports:[ConfigModule,redisModule],
   controllers: [PollsController],
   providers: [PollsService],
 })
