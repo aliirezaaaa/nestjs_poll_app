8d7b9f2ded799d1ecfba0d30677d450769198ea2
Alireza Khorramshekooh <alireza.khorramshekooh@gmail.com>
2024-11-23 21:21:08 +0330

    structure refactor completely

---
+++ server/src/polls/types.ts
@@ -0,0 +1,42 @@
+import { Request } from '@nestjs/common';
+
+// service types
+export type CreatePollFields = {
+  topic: string;
+  votesPerVoter: number;
+  name: string;
+};
+
+export type JoinPollFields = {
+  pollID: string;
+  name: string;
+};
+
+export type RejoinPollFields = {
+  pollID: string;
+  userID: string;
+  name: string;
+};
+
+// repository types
+export type CreatePollData = {
+  pollID: string;
+  topic: string;
+  votesPerVoter: number;
+  userID: string;
+};
+
+export type AddParticipantData = {
+  pollID: string;
+  userID: string;
+  name: string;
+};
+
+// guard types
+type AuthPayload = {
+  userID: string;
+  pollID: string;
+  name: string;
+};
+
+export type RequestWithAuth = Request & AuthPayload;
